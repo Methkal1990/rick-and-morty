18b836d8ee761c9896985ab0d3e3aed60e39f1bb
methkal <methkal.khalawi@rakuten.com>
2022-03-05 20:26:02 +0100

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Auth from "./pages/Auth/index.jsx";
+import Character from "./pages/Character/index.jsx";
+import Home from "./pages/Home/index.jsx";
+
 function App() {
-  return <div>app</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/auth" element={<Auth />} />
+        <Route exact path="/:characterId" element={<Character />} />
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
