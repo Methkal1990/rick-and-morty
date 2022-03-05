4e81ed27839e945016d1b18b916485080b817a67
methkal <methkal.khalawi@rakuten.com>
2022-03-05 20:10:39 +0100

    frontend and redux setup

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Root from "./Root.jsx";
+import App from "./App.jsx";
+
+ReactDOM.render(
+  <Root>
+    <App />
+  </Root>,
+  document.getElementById("root")
+);
